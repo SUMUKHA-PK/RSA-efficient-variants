b6f76f7e457fb374c4df6daad99c04a11c92ba0d
Sumukha Pk <sumukhapk46@gmail.com>
2018-10-27 22:55:57 +0530

    Minor changes in main

--- src/EAMRSA/main.py
+++ src/EAMRSA/main.py
@@ -51,3 +51,32 @@ def phi(arr):
 
 print('phi(N) = ',end="")
 print(phi(primes_p))
+
+e = 65537
+
+def multiplicative_inverse(e, phi):
+    d = 0
+    x1 = 0
+    x2 = 1
+    y1 = 1
+    temp_phi = phi
+    
+    while e > 0:
+        temp1 = temp_phi/e
+        temp2 = temp_phi - temp1 * e
+        temp_phi = e
+        e = temp2
+        
+        x = x2- temp1* x1
+        y = d - temp1 * y1
+        
+        x2 = x1
+        x1 = x
+        d = y1
+        y1 = y
+    return d + phi
+
+d = multiplicative_inverse(e,phi(primes_p))
+
+print('Value of d is : ',end="")
+print(d)
