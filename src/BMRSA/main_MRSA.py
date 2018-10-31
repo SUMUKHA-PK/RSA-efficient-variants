8379cdd7e38cbae06e9903e77ca5a8b7d1682c32
16CO234 (Prajval M) <26prajval98@gmail.com>
2018-10-31 15:26:49 +0530

    Almost Done

--- src/BMRSA/main_MRSA.py
+++ src/BMRSA/main_MRSA.py
@@ -57,8 +57,8 @@ def crt(m, x):
     return x[0]
 
 
-def generate_message(l):
-    return np.array([randint(0, 128) for _ in range(l)], dtype=object)
+def generate_message(m):
+    return randint(0, min(m, 128))
 
 
 def is_prime(n, d):
@@ -73,7 +73,7 @@ def is_prime(n, d):
                 break
 
         if prime:
-            d.append(prime)
+            d.append(i)
 
         i += 2
 
@@ -91,13 +91,14 @@ def generate_primes(n, b):
     primes = []
     bits = int(n/b)
     minimum = 2 ** (bits-1)
-    maximum = 2 ** (bits)
+    maximum = 2 ** bits
 
     d = [2, 3, 5, 7]
 
     for i in range(minimum + 1, maximum):
         if is_prime(i, d):
             primes.append(i)
+
         if len(primes) >= 2*b:
             break
 
@@ -112,83 +113,13 @@ def generate_primes(n, b):
     return primes[:b]
 
 
-def encrypt(message, es, n):
-    return [(message[i] ** es[i]) % n for i in range(len(message))]
-
-
-def interchange(tree):
-    for i in range(1, len(tree), 2):
-        tree[i], tree[i+1] = swap(tree[i], tree[i+1])
-
-
-def generate_tree_e(es):
-    leaves = len(es)
-    t = int(log(leaves, 2))
-
-    t = 2*(leaves - 2**t)
-
-    tree = [*es[t:], *es[:t]]
-
-    length = 2**(int(log(leaves, 2))+1) + t - 1
-
-    i = len(tree) - 1
-
-    while len(tree) < length:
-        tree = [tree[i] * tree[i-1], *tree]
-        i -= 1
-
-    # interchange(tree)
-
-    return tree
-
-
-def generate_tree_v(vs, tree_es, n):
-    leaves = len(vs)
-    t = int(log(leaves, 2))
-
-    t = 2 * (leaves - 2 ** t)
-
-    tree = [*vs[t:], *vs[:t]]
-
-    length = 2 ** (int(log(leaves, 2)) + 1) + t - 1
-
-    tree = [*[0 for _ in range(length - len(tree))], *tree]
-
-    i = len(tree) - len(vs) - 1
-
-    while i >= 0:
-        left = 2*i + 1
-        right = 2*i + 2
-        tree[i] = ((tree[left]**tree_es[right])*(tree[right]**tree_es[left])) % n
-        i -= 1
-
-    return tree
-
-
-def generate_r(tree_es, tree_vs, n, r):
-
-    i = 0
-    tree = [r, *[0 for _ in range(len(tree_vs)-1)]]
-
-    while 2*i + 2 < len(tree):
-        left = 2*i + 1
-        right = 2*i + 2
-        t = crt([tree_es[left], tree_es[right]], [0, 1])
-        t_l = int(t//tree_es[left])
-        t_r = int((t-1)//tree_es[right])
-        print(n, tree[i], tree_vs[left], tree_vs[right], t, t_l, t_r)
-        r_r = ((tree[i]**t)//((tree_vs[right]**t_l)*(tree_vs[left]**t_r)) % n)
-        r_l = (r // r_r) % n
-        tree[left] = r_l
-        tree[right] = r_r
-        i += 1
-    return tree
+def encrypt_decrypt(message, e, n):
+    return (message ** e) % n
 
 
 def main():
     n = int(sys.argv[1])
     b = int(input("Enter b\n"))
-    li = int(input("\nEnter l\n"))
     primes = generate_primes(n, b)
 
     n = np.prod(primes)
@@ -197,65 +128,36 @@ def main():
     for i in primes:
         phi_n *= (i-1)
 
-    temp = randint(2, 100)
-    es = []
+    temp = randint(2, min(100, phi_n - 1))
 
-    while len(es) < li:
+    while True:
+        is_rp = False
         if gcd(temp, phi_n) == 1:
             is_rp = True
-            for i in es:
-                if gcd(temp, i) != 1:
-                    is_rp = False
-                    break
-            if is_rp:
-                es.append(temp)
+        if is_rp:
+            break
         temp += 1
 
-    ds = np.array([MMI(num, phi_n) for num in es], dtype=object)
+    e = temp
 
-    d = np.prod(ds) % phi_n
+    d = MMI(e, phi_n) % phi_n
 
     # private key
     # Check
-    ds = [(d % p) - 1 for p in primes]
-
-    message = generate_message(li)
+    message = generate_message(n)
 
     # encrypted_messages (Ciphers)
-    cs = encrypt(message, es, n)
+    c = encrypt_decrypt(message, e, n)
 
-    # Percolate Up
-    e = 1
-    for i in es:
-        e *= i
-    e = e % n
+    # decryption
+    de = encrypt_decrypt(c, d, n)
 
-    vs = cs[:]
-    v = 1
-    # Root node
-    for i in range(len(vs)):
-        v *= vs[i] ** int((e/es[i]))
+    print("message      : ", message)
+    print("cipher       : ", c)
+    print("decrypted    : ", de)
 
-    v %= n
-
-    # Exponentiation
-    cps = [v % p for p in primes]
-    mps = [(cps[i] ** ds[i]) % primes[i] for i in range(len(primes))]
-
-    ys = [int(n/p) for p in primes]
-    nis = [ys[i] * MMI(ys[i], primes[i]) for i in range(len(ys))]
-
-    r = 0
-
-    for i in range(len(nis)):
-        r += nis[i] * mps[i]
-
-    tree_es = generate_tree_e(es)
-    tree_vs = generate_tree_v(vs, tree_es, n)
-
-    tree_rs = generate_r(tree_es, tree_vs, n, r)
-
-    print(tree_rs, message)
+    if message == de:
+        print("CORRECT")
 
 
 if __name__ == '__main__':
