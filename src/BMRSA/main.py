8fce32bf2cad7be9d8129d07a818e5d242e9b1a8
16CO234 (Prajval M) <26prajval98@gmail.com>
2018-11-13 18:27:52 +0530

    Done

--- src/BMRSA/main.py
+++ src/BMRSA/main.py
@@ -4,13 +4,24 @@ import matplotlib.pyplot as plt
 
 from useful import funcs
 
+
+# number of bs
 bsize = 1
 
+
 while bsize < 4:
+
+    # Start from prime size = 10 bits
+    # mrsa and bmrsa time
     itr = 10
     mrsa = []
     bmrsa = []
+
+    # points needed to be marked on graph
     pts = []
+
+    # go till 18 bits
+    # No of bits in key = 2 ** (n/b) * b
     while itr <= 18:
 
         pts.append(2 ** itr)
@@ -32,25 +43,38 @@ while bsize < 4:
 
         timeMRSA = 0
 
+        # Get time for each MRSA
         for i in range(ll):
             timeMRSA += Mrsa.main(primes, es[i], m[i])
 
+        # Time in milliseconds
         mrsa.append(timeMRSA*1000)
 
+        # Get time for BMRSA
         timeBMRSA = Bmrsa.main(primes, es, m)
 
+        # Time in milli seconds
         bmrsa.append(timeBMRSA*1000)
 
         itr += 2
 
+    # Plotting graphs
     fig, ax = plt.subplots()
+
+    # plot subplots
     ax.plot(pts, mrsa, "ro-", label="MRSA")
     ax.plot(pts, bmrsa, "bo-", label="BMRSA")
 
+    # legends in graph
     legend = ax.legend(loc='upper center', shadow=True, fontsize='x-large')
 
+    # title
     plt.title("Decryption time vs Key Size in bits for batch size = " + str(bsize))
     plt.xlabel('Time in milliseconds')
     plt.ylabel('No of Bits in key')
+
+    # display graph
     plt.show()
+
+    # increase byte size
     bsize += 1
