de736128bdc3ddc66564ab8eaea06323e4bbd8b2
16CO234 (Prajval M) <26prajval98@gmail.com>
2018-11-13 17:50:10 +0530

    Done

--- src/BMRSA/useful/funcs.py
+++ src/BMRSA/useful/funcs.py
@@ -3,6 +3,7 @@ import sys
 from random import randint
 from math import gcd
 
+# function to find the modulo inverse of a number mod n
 MMI = lambda a, n, s=1, t=0, z=0: (n < 2 and t % z or MMI(n, a % n, t, s - a // n * t, z or n), -1)[n < 1]
 
 
@@ -53,8 +54,10 @@ def crt(m, x):
     return x[0]
 
 
+# function to find if a number n is prime looking at dictionary of primes d
 def is_prime(n, d):
 
+    # generating primes till n
     i = d[-1] + 2
     while i*i <= n:
         prime = True
@@ -69,24 +72,36 @@ def is_prime(n, d):
 
         i += 2
 
+    # checking if any of primes divide n
     for j in d:
         if n % j == 0:
+            # if n is itself a prime
             if n == j:
                 return True
             else:
                 return False
 
+    #  n is prime
     return True
 
 
+# generating b primes of n/b bits
 def generate_primes(n, b):
+
+    # initially no primes present
     primes = []
     bits = int(n/b)
+
+    # start from the first number which is n/b bits long
     minimum = 2 ** (bits-1)
+
+    # till last number n/b bits long
     maximum = 2 ** bits
 
+    # basic dictionary
     d = [2, 3, 5, 7]
 
+    # finding if any of the number is prime and stopping if more than 2*b primes are acquired
     for i in range(minimum + 1, maximum):
         if is_prime(i, d):
             primes.append(i)
@@ -95,31 +110,40 @@ def generate_primes(n, b):
 
     primes = np.array(primes, dtype=object)
 
+    # if less than b primes present then stop
     if primes.size < b:
         print("b is too large. Change b or n")
         sys.exit(-1)
 
+    # randomise primes and select first b of it
     np.random.shuffle(primes)
 
     return primes[:b]
 
 
+# swap two numbers
 def swap(a, b):
     return b, a
 
 
+# Generate message of of length l
 def generate_message(l, n):
     return np.array([randint(1, min(128, n)) for _ in range(l)], dtype=object)
 
 
+#  Generate es which are co-prime to phi and themselves
 def es(ps, li):
+
+    # phi value is (p1-1)*(p2-1)*....(pn-1)
     phi = 1
     for ii in ps:
         phi *= (ii - 1)
 
-    temp = randint(2, 100)
+    # start from a random value
+    temp = randint(2, 5)
     ess = []
 
+    # finding numbers co prime to and themselves
     while len(ess) < li:
         if gcd(temp, phi) == 1:
             is_rp = True
@@ -131,4 +155,5 @@ def es(ps, li):
                 ess.append(temp)
         temp += 1
 
+    # return array of co primes to phi
     return ess
